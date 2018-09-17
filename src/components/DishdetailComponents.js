d5b774c87eeb5cd7c9bdb1ee196a56d149b37a3b
rh01 <1048157315@qq.com>
2018-09-17 14:29:05 +0800

    Reformat DishdetailComponents

---
+++ src/components/DishdetailComponents.js
@@ -0,0 +1,26 @@
+import React, {Component} from "react";
+import  { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle, Row, CardGroup } from 'reactstrap'
+
+
+class DishDetail extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state ={};
+    }
+
+   
+
+    render() {
+        
+        const menu = this.props.dishes.map((dish) => {
+            
+        });
+            
+
+    }
+}
+
+
+export default DishDetail;

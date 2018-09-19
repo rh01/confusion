6802293b5a7000742d7b961d62199e045ef417fb
rh01 <1048157315@qq.com>
2018-09-19 10:42:03 +0800

    Single Page Application 2.

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Card, CardImg, CardText, CardBody, CardTitle} from 'reactstrap';
+import  { Card, CardImg, CardImgOverlay, CardBody, CardText, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom'
 
     /**
      * 
@@ -27,10 +28,10 @@ import {Card, CardImg, CardText, CardBody, CardTitle} from 'reactstrap';
      * 
      * @param {object} dish 
      */
-    function RenderComments({dish}) {
+    function RenderComments({comments}) {
         const options = {year: 'numeric', month: 'long', day: 'numeric'};
-        if(dish != null) {
-            const comments = dish.comments.map((comment) => {
+        if(comments != null) {
+            const comments_processed = comments.map((comment) => {
                 return (
               
                         <div key={comment.id}>
@@ -49,7 +50,7 @@ import {Card, CardImg, CardText, CardBody, CardTitle} from 'reactstrap';
             return (
                 <div>
                     <h4>Comments</h4>
-                    {comments}
+                    {comments_processed}
                 </div>
             );
         } else {
@@ -65,11 +66,21 @@ import {Card, CardImg, CardText, CardBody, CardTitle} from 'reactstrap';
         return (
             <div className="container">
                 <div className="row">
-                    <div className="col-sm-12 col-md-5 m-1">
-                        <RenderDish  dish={props.dish}/>
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to='/menu'>Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr />
                     </div>
-                    <div className="col-sm-12 col-md-5 m-1">
-                        <RenderComments dish={props.dish} />
+                </div>
+                <div className="row">   
+                    <div className="col-12 col-md-5 m-1">  
+                        <RenderDish  dish={props.dish}/>
+                    </div> 
+                    <div className="col-12 col-md-5 m-1">  
+                        <RenderComments comments={props.comments} />
                     </div>
                 </div>
             </div>
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,6 +10,7 @@ import { PROMOTIONS } from '../shared/promotions';
 import { LEADERS } from '../shared/leaders';
 
 import {Switch, Route, Redirect} from 'react-router-dom';
+import DishDetail from './DishdetailComponent';
 
 class Main extends Component {
 
@@ -40,8 +41,20 @@ class Main extends Component {
                     promotion = {this.state.promotions.filter((promo) => promo.featured)[0]}
                     leader = {this.state.leaders.filter((leader) => leader.featured)[0]}
                 />
-                )
+                );
         }
+
+        const DishWithId = ({match}) => {
+            return(
+                <DishDetail dish={this.state.dishes.filter((dish)=> dish.id === parseInt(match.params.dishId, 10))[0]} 
+                    comments={this.state.comments.filter((comment) => comment.dishId=== parseInt(match.params.dishId, 10))}
+                    />
+            );
+        }
+
+
+
+
 
         return (
         <div>
@@ -50,6 +63,7 @@ class Main extends Component {
            <Switch>
                <Route path="/home" component={HomePage} />
                <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+               <Route path="/menu/:dishId" component={DishWithId} />
                <Route exact path='/contactus' component={Contact} />} />
                <Redirect to="/home" />
            </Switch>

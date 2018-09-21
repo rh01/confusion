970f0e9e56dbff16b782f7742a74c22ee44eb255
rh01 <1048157315@qq.com>
2018-09-21 10:19:10 +0800

    React Redux Forms Revisited.

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -7,7 +7,7 @@ function About(props) {
     const leaders = props.leaders.map((leader) => {
         return (
             
-            <div className="row row-content">
+            <div key={leader.id} className="row row-content">
                 <Media>
                     {/* <div className="col-12 col-md-2"> */}
                         <Media left top>
---
+++ src/redux/forms.js
@@ -0,0 +1,10 @@
+export const InitialFeedback = {
+    firstname: '',
+    lastname : '',
+    telname : '',
+    email : '',
+    agree : false,
+    contactType : 'Tel.',
+    message: ''
+
+}

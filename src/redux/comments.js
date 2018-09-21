4e38d46c6d3d66b8b69aa6d45a7b8cecda243a45
rh01 <1048157315@qq.com>
2018-09-21 11:27:49 +0800

    Fetch from Server.

--- src/redux/comments.js
+++ src/redux/comments.js
@@ -2,13 +2,20 @@ import { COMMENTS } from '../shared/comments';
 import * as ActionType from './ActionTypes';
 
 
-export const Comments = (state =COMMENTS, action) => {
+export const Comments = (state ={
+    errMess: null,
+    comments : []
+}, action) => {
     switch (action.type) {
         case ActionType.ADD_COMMENT:
             var comment = action.payload;
             comment.id = state.length;
             comment.data = new Date().toISOString();
-            return state.concat(comment);
+            return {...state,comments: state.comments.concat(comment)};
+        case ActionType.ADD_COMMENTS:
+            return {...state, errMess: null, comments: action.payload};
+        case ActionType.COMMENTS_FAILED:
+            return {...state, errMess:null, comments: action.payload};
     
         default:
             return state;
